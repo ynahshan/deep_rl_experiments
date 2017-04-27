0399fa7f819743d8235b6c7d9a3b2e1c9ebbd701
ynahshan <yury.nahshan@intel.com>
2017-04-27 16:21:55 +0300

    compare methods

--- myRlLearning/grid_world_agents_compare.py
+++ myRlLearning/grid_world_agents_compare.py
@@ -15,31 +15,28 @@ from monte_carlo_agent import MonteCarloAgent
 from sarsa_agent import SarsaAgent
 from qlearning_agent import QLearningAgent
 
-agents = ["simple", "policy_it", "monte_carlo", "sarsa", "qlearning"]
+np.random.seed(0)
+GAMMA = 0.7
+ALPHA = 0.8
 
-CONVERGENCE_LIMIT = 10e-4
-
-def create_agent(env, agent_type, verbosity=0):
+def create_agent(env, agent_type, gamma, alpha, verbosity=0):
     if agent_type == "simple":
         agent = SimpleValueTableAgent(env.num_states)
     elif agent_type == "policy_it":
         agent = PolicyIterationAgent(env.num_states, env.all_actions())
     elif agent_type == "monte_carlo":
-        agent = MonteCarloAgent(eps=1.0, gamma=0.8, verbose=verbosity >= 2)
+        agent = MonteCarloAgent(gamma=gamma, verbose=verbosity >= 2)
     elif agent_type == "sarsa":
-        agent = SarsaAgent(verbose=verbosity >= 2)
+        agent = SarsaAgent(gamma=gamma, alpha=alpha, verbose=verbosity >= 2)
     elif agent_type == "qlearning":
-        agent = QLearningAgent(verbose=verbosity >= 2)
+        agent = QLearningAgent(gamma=gamma, alpha=alpha, verbose=verbosity >= 2)
         
     return agent    
 
-np.random.seed(0)
-if __name__ == '__main__':
-    # Prepare Agent
-    verbosity = 1  # 0 - no verbosity; 1 - show prints between episodes; 2 - show agent log
-    env_factory = EnvironmentFactory(EnvironmentFactory.EnvironmentType.RandomPlayer)
+def train_agent(agent_name, env_type, gamma, alpha, verbosity=1):
+    env_factory = EnvironmentFactory(env_type)
     env = env_factory.create_environment()
-    agent = create_agent(env, agents[4], verbosity=verbosity)
+    agent = create_agent(env, agent_name, gamma, alpha, verbosity=verbosity)
     solver = GridWorldSolver(env_factory, agent)
     print("Evaluate %s performance on %s grid world\n" % (agent.__class__.__name__, env.__class__.__name__))
     if verbosity >= 1:
@@ -49,14 +46,15 @@ if __name__ == '__main__':
     
     start_time = timeit.default_timer()
 
-    save_model = True
+    save_model = False
     converged = False
     rewards = []
     total_steps = 0
     total_iterations = 0
     convergence_count = 0
+    CONVERGENCE_LIMIT = 10e-4
     CONVERGENCE_STOP_COUNT = 2
-#     while total_iterations < 7:
+
     while not converged:
         print("[%d] Train agent with all possible states" % total_iterations)
         steps = solver.train(range(env.num_states), verbosity)
@@ -90,7 +88,34 @@ if __name__ == '__main__':
     print("Saving V table to vtable.bin")
     if save_model:
         agent.save_model('vtable.bin')
+        
+    return rewards, total_iterations, total_steps
 
-    plt.plot(rewards, 'g')
+if __name__ == '__main__':
+    # Prepare Agent
+    verbosity = 0  # 0 - no verbosity; 1 - show prints between episodes; 2 - show agent log
+    env_type = EnvironmentFactory.EnvironmentType.RandomPlayerAndGoal
+#     agents = ["simple", "policy_it", "monte_carlo", "sarsa", "qlearning"]
+#     agents = ["monte_carlo", "sarsa", "qlearning"]
+    agents = ["sarsa", "qlearning"]
+
+    res = {}
+    max_it = -1
+    for agent in agents:
+        res[agent] = train_agent(agent, env_type, gamma=GAMMA, alpha=ALPHA, verbosity=verbosity)
+        if res[agent][1] > max_it:
+            max_it = res[agent][1]
+    
+    print()    
+    fig, ax = plt.subplots()
+    for agent in agents:
+        rewards = res[agent][0]
+        it = res[agent][1]
+        steps = res[agent][2]
+        print("Agent: %s, training iterations: %d, total steps %d, final reward %f" % (agent, it, steps, rewards[-1:][0]))
+        rewards = np.append(rewards, [rewards[-1:][0]] * (max_it - len(rewards)))
+        ax.plot(rewards, label="%s, %d iter, %d steps" % (agent, it, steps))
+    
+    ax.legend(loc='lower right', shadow=True)
     plt.show()
     print('Done')
